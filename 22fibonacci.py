7f79427ba83a018da898aa3ceac9d49dced2a977
laurengyn <launguy@ucdavis.edu>
2025-02-01 00:56:56 -0800

    new

---
+++ 22fibonacci.py
@@ -0,0 +1,5 @@
+'''
+A classic programming interview question is to write a program that reports the first 10 numbers from the 
+Fibonacci sequence: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34. 
+This is a tricky problem. You need to initialize and keep track of 2 previous values.
+'''
---
+++ 23triples.py
@@ -0,0 +1,6 @@
+'''
+Write a program that finds all Pythagorean triples for triangles with sides a and b less than 100. 
+For example, 3, 4, 5 is a triple: 3^2 + 4^2 = 5^2. 
+Hint: all sides, including the hypotenuse, must be integers. 
+A good way to test for an integer is like: if c % 1 == 0.
+'''
