031c14336daba9954d6a826b6b995e472a82bf0d
laurengyn <launguy@ucdavis.edu>
2025-02-01 00:35:03 -0800

    new

--- 20demo.py
+++ 20demo.py
@@ -83,3 +83,55 @@ def poisson (n, k):
     return (n ** k) * (math.e ** -n) / factorial(k)
 
 print (poisson(2, 3)) ## should equal 0.18044704431548356
+
+'''
+Write a function that solves "n choose k": n! / k!(n - k)!
+'''
+def nchoosek (n, k):
+    return factorial(n) / (factorial(k) * factorial(n - k))
+
+print (nchoosek(5, 2)) ## should equal 10
+
+'''
+Write a function that estimates Euler's number: 
+e (2.71828...). This can be computed as the infinite sum of 1/n!. 
+Choose a finite number of iterations.
+
+note to self: check w ta if this is the correct interpretation of the problem
+'''
+
+def euler (n):
+    sum = 0
+    for i in range (n + 1):
+        sum = sum + 1 / factorial (i)
+    return sum
+
+print (euler(10)) ## should equal 2.7182815255731922
+
+'''
+Write a function to determine if a number is prime.
+'''
+
+def is_prime (n):
+    for i in range (2, n): 
+        if n % i == 0: return False
+    return True
+
+print (is_prime(7)) ## should return True
+print (is_prime(8)) ## should return False
+
+'''
+Write a function that estimates Pi using the Nilakantha series. 
+Again, choose a finite limit.
+'''
+
+def calc_pi(n):
+    pi = 3
+    for i in range(1, n+1):
+        n = 2 * i
+        d = n * (n+1) * (n+2)
+        if i % 2 == 0: pi = pi - 4 / d
+        else:          pi = pi + 4 / d
+    return pi
+
+print (calc_pi(10)) ## should equal 3.141592653589793
