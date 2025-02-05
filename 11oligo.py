76264072af24a2938a39306b6a40bf7cf3b85705
laurengyn <launguy@ucdavis.edu>
2025-02-04 20:51:42 -0800

    new

--- 11oligo.py
+++ 11oligo.py
@@ -6,12 +6,12 @@ For longer oligos, Tm = 64.9 + 41*(G+C -16.4) / (A+T+G+C)
 Demonstrate that your program works by computing the Tm of several oligos of different sizes.
 """
 
-def Tm(A, C, G, T):
-    if (A+C+G+T) <= 13: return ((A+T)*2 + (G+C)*4)
-    else: return 64.9 + 41*(G+C -16.4) / (A+T+G+C)
+def tm(a, c, g, t):
+    if (a+c+g+t) <= 13: return ((a+t)*2 + (g+c)*4)
+    else: return 64.9 + 41*(g+c -16.4) / (a+t+g+c)
 
-print (Tm(1, 1, 1, 1))
-print (Tm(3, 3, 3, 3))
-print (Tm(3, 3, 3, 4))
-print (Tm(3, 3, 4, 4))
-print (Tm(4, 4, 4, 4))
+print (tm(1, 1, 1, 1))
+print (tm(3, 3, 3, 3))
+print (tm(3, 3, 3, 4))
+print (tm(3, 3, 4, 4))
+print (tm(4, 4, 4, 4))
