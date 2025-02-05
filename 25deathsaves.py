c7383be802928ef78d968f3e73b2f86cb29450bf
laurengyn <launguy@ucdavis.edu>
2025-02-04 16:23:55 -0800

    new

--- 25deathsaves.py
+++ 25deathsaves.py
@@ -9,3 +9,34 @@ If you are unlucky and roll a 1, it counts as 2 failures.
 If you're lucky and roll a 20, you gain 1 health and have "revived". 
 Write a program that simulates death saves. What is the probability one dies, stabilizes, or revives?
 '''
+
+import random
+import math
+
+def death_saves():
+    success = 0
+    fail = 0
+    while success < 3 and fail < 3:
+        roll = random.randint(1, 20)
+        print (roll)
+        if roll == 1:
+            fail += 2
+            print ('Failed twice, fail count', fail)
+        elif roll == 20:
+            success = 3
+            print ("You have revived")
+        elif roll < 10:
+            fail += 1
+            print ('Fail count', fail)
+        elif roll >= 10:
+            success += 1
+            print ('Success count', success)
+            if success == 3:
+                print ("You are stable but unconscious")
+        else:
+            print ("Neither success nor fail")
+    if fail == 3:
+        print ("You die")
+
+death_saves()
+    
